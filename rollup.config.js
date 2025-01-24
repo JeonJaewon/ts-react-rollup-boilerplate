90e93782caa63993106015270b2326e327a05293
Jaewon Jeon <wksk04515@naver.com>
2025-01-24 18:40:40 +0900

    configure SWC for TypeScript and update Rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,18 @@
-import babel from "@rollup/plugin-babel";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "@rollup/plugin-typescript"
+import swc from "rollup-plugin-swc3"
 
 export default {
   input: "./src/index.ts",
   output: {
     file: "./dist/index.js",
     format: "es",
-    sourcemap: true,
   },
   plugins: [
-    babel({
-      babelHelpers: "bundled",
-      presets: [
-        "@babel/preset-env",
-        "@babel/preset-react",
-        "@babel/preset-typescript",
-      ],
+    swc({
+      exclude: /node_modules/, // default
+      tsconfig: "tsconfig.json", // default
       extensions: [".js", ".jsx", ".ts", ".tsx"],
     }),
     typescript(),
   ],
-};
+}
